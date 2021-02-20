b667c1de237b8ceecaedbc17a0440465eeb06b9b
m0r0z0vd <morozov.symfony@gmail.com>
2021-02-20 16:03:14 +0200

    Fixed functional test

--- tests/Functional/ItemControllerTest.php
+++ tests/Functional/ItemControllerTest.php
@@ -2,36 +2,47 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\Item;
 use App\Repository\ItemRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
 
 class ItemControllerTest extends WebTestCase
 {
+    private const EXISTING_USERNAME = 'john';
+
+    /** @var KernelBrowser */
+    private $client;
+
+    /** @var ItemRepository */
+    private $itemsRepository;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+        $this->itemsRepository = static::$container->get(ItemRepository::class);
+    }
+
     public function testCreate()
     {
-        $client = static::createClient();
-
         $userRepository = static::$container->get(UserRepository::class);
-        $itemRepository = static::$container->get(ItemRepository::class);
-        $entityManager = static::$container->get(EntityManagerInterface::class);
+        $user = $userRepository->findOneBy(['username' => self::EXISTING_USERNAME]);
+        $this->client->loginUser($user);
 
-        $user = $userRepository->findOneByUsername('john');
-
-        $client->loginUser($user);
-        
-        $data = 'very secure new item data';
-
+        $data = 'very secure new item data ' . microtime(true);
         $newItemData = ['data' => $data];
-
-        $client->request('POST', '/item', $newItemData);
-        $client->request('GET', '/item');
+        $this->client->request('POST', '/item', $newItemData);
 
         $this->assertResponseIsSuccessful();
-        $this->assertStringContainsString('very secure new item data', $client->getResponse()->getContent());
+        $content = $this->client->getResponse()->getContent();
+        $this->assertJson($content);
+        $content = json_decode($content, true);
+        $this->assertEquals([], $content);
 
-        $userRepository->findOneByData($data);
+        $lastUserItem = $this->itemsRepository->findOneBy(['user' => $user], ['id' => 'DESC']);
+        $this->assertInstanceOf(Item::class, $lastUserItem);
+        $this->assertEquals($data, $lastUserItem->getData());
     }
 }
